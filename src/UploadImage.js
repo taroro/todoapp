710756f428e926fc90045249bd2982d358e6ae2c
unknown <artid.boonrerng@gmail.com>
2021-06-12 11:43:10 +0700

    add to post

--- src/UploadImage.js
+++ src/UploadImage.js
@@ -1,23 +1,43 @@
 import React, { useState, useEffect } from "react";
 import firebase from "./firebase";
+import TextField from "@material-ui/core/TextField";
 function UploadImage() {
     const [image , setImage] = useState('');
     const [imageUrl, setImageUrl] = useState('');
+    const [caption, setCaption] = useState("");
     var storage = firebase.storage();
     const upload = ()=>{
         if(image == null) return;
-        storage.ref(`/images/${image.name}`).put(image) .on("state_changed" , 
+        console.log(image)
+        storage.ref(`/images/${image.name}`).put(image).on("state_changed",
           () => {
+            /* let url = storage.ref(`/images/${image.name}`).getDownloadURL()
+            setImageUrl(url) */
+
             storage.ref(`/images/${image.name}`).getDownloadURL().then((url) => {
               setImageUrl(url)
+            }).then(() => {
+              firebase.firestore().collection('post').add({
+                caption: caption,
+                imageUrl: imageUrl,
+                like: 0,
+                dislike: 0
+              })
             })
-          } , 
-          alert('ERROR')
+          }
         );
       }
       return (
         <div className="App">
           <center>
+          <TextField
+                style={{ width: "100%" , padding: 8 }}
+                id="standard-basic"
+                value={caption}
+                onChange={(e) => setCaption(e.target.value)}
+                label="Add Todo"
+                variant="outlined"
+            />
           <input type="file" onChange={(e)=>{setImage(e.target.files[0])}}/>
           <button onClick={upload}>Upload</button>
           <img src={imageUrl} />
