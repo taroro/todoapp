e32dccd5f66f23346727d6ed3ad1025cff4c0caa
unknown <artid.boonrerng@gmail.com>
2021-06-12 12:22:50 +0700

    dislike button

--- src/Instragoo.js
+++ src/Instragoo.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import UploadImage from "./UploadImage";
 import Switch from "@material-ui/core/Switch"
 import IconButton from "@material-ui/core/IconButton";
-import DeleteIcon from "@material-ui/icons/Delete"
+import ThumbUpIcon from "@material-ui/icons/ThumbUp"
+import ThumbDownIcon from "@material-ui/icons/ThumbDown"
 import firebase from "./firebase";
 function Instragoo() {
     const [posts, setPosts] = useState([])
@@ -22,15 +23,26 @@ function Instragoo() {
     const like = (id, numLike) => {
         firebase.firestore().collection('post').doc(id).set({like: numLike+1}, { merge: true });
     }
+
+    const dislike = (id, numLike) => {
+        firebase.firestore().collection('post').doc(id).set({dislike: numLike+1}, { merge: true });
+    }
+
     return (
         <>
             <UploadImage />
             {posts.map((post, i) => (
-                <div key={i} style={{ display: "flex", flexDirection: "col", alignItems: "center"}}>
-                    <img src={post.imageUrl} />
+                <div key={i} style={{ display: "flex", flexDirection: "row", alignItems: "center", margin: 10}}>
+                    <img src={post.imageUrl} width="100" />
                     <p style={{padding: 8, flexGrow: 1}}><b>{post.caption}</b></p>
-                    <button onClick={e => like(post.id, post.like)}>LIKE</button>
+                    <IconButton aria-label="Like" onClick={e => like(post.id, post.like)} >
+                        <ThumbUpIcon fontSize="large" />
+                    </IconButton>
                     <p style={{padding: 8, flexGrow: 1}}><b>{post.like}</b></p>
+                    <IconButton aria-label="Dislike" onClick={e => dislike(post.id, post.dislike)} >
+                        <ThumbDownIcon fontSize="large" />
+                    </IconButton>
+                    <p style={{padding: 8, flexGrow: 1}}><b>{post.dislike}</b></p>
                 </div>
             ))}
         </>
